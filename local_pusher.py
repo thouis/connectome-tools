6fadb52efcb634899ef7056757a3855e69c23329
Thouis (Ray) Jones <thouis@seas.harvard.edu>
2012-10-09 14:50:47 -0400

    remote and local pushers

---
+++ local_pusher.py
@@ -0,0 +1,78 @@
+import os
+import sys
+import glob
+import subprocess
+import shutil
+import time
+
+# include trailing slashes in paths
+SSD_dir = '/cygdrive/v/LGNS1_Montages/'
+Local_dir = '/cygdrive/f/LGNS1_Montages/'
+Remote_dir = '/cygdrive/x/joshm/LGNs1/rawMontages/'
+
+def rsync(from_dir, to_dir, *args):
+    result = subprocess.call(["/usr/bin/rsync",
+                              "-a", "--no-perms"] + list(args) +
+                             [from_dir,
+                              to_dir])
+    return (result == 0)  # zero exit on success
+
+def write_md5s(path):
+    try:
+        tif_files = glob.glob(os.path.join(path, "*.tif"))
+        result = subprocess.check_output(["/usr/bin/md5sum"] + tif_files)
+        f = open(os.path.join(path, "MD5_CHECKSUMS.txt"), "w")
+        f.write(result)
+        f.close()
+    except Exception, e:
+        print "Could not write MD5 checksums", e
+
+def delay():
+    # Wait 60 seconds
+    print "Sleeping for 60 seconds",
+    sys.stdout.flush()
+    for i in range(6):
+        time.sleep(10)
+        print "...%d" % (60 - ((i + 1) * 10)),
+        sys.stdout.flush()
+    print ""
+
+
+if __name__ == '__main__':
+    while True:
+        # Search the SSD for directories that have finished
+        finished_files = glob.glob(os.path.join(SSD_dir, '*', 'finished.mat'))
+        finished_dirs = [os.path.dirname(f) for f in finished_files]
+        if finished_dirs:
+            print "Will remove these directories after verifying checksums:"
+            print "    ", "\n    ".join(finished_dirs)
+
+        # First, sync the SSD to the Local
+        print "Syncing %s to %s..." % (SSD_dir, Local_dir)
+        success = rsync(SSD_dir, Local_dir, "-q", "-W", "--progress", "-h")
+        if not success:
+            print "Syncing SSD to Local sync failed.  Retrying, not removing anything."
+            delay()
+            continue
+
+        # Create md5 checksums for every .tif file in the finished directories
+        for d in finished_dirs:
+            write_md5s(d)
+
+        # Then, resync the SSD to any finished directories, but use checksumming
+        # instead of size and modification time.
+        for d in finished_dirs:
+            databuffer_subdir = os.path.join(Local_dir, os.path.split(d)[1], '')
+            "Checksum-based sync of %s to %s..." % (d, databuffer_subdir)
+            success = rsync(os.path.join(d, ''), databuffer_subdir, "-c", "-vv")
+            if not success:
+                print "    Sync failed, not removing %s" % (d)
+            else:
+                print "    Removing %s" % (d)
+                try:
+                    shutil.rmtree(d)
+                    print "Would remove %s" % d
+                except:
+                    print "Could not remove finished directory %d" % (d)
+
+        delay()
---
+++ remote_pusher.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import glob
+import subprocess
+import shutil
+import time
+
+# include trailing slashes in paths
+SSD_dir = '/cygdrive/v/LGNS1_Montages/'
+Local_dir = '/cygdrive/f/LGNS1_Montages/'
+Remote_dir = '/cygdrive/x/joshm/LGNs1/rawMontages/'
+
+def rsync(from_dir, to_dir, *args):
+    result = subprocess.call(["/usr/bin/rsync",
+                              "-a", "--no-perms"] + list(args) +
+                             [from_dir,
+                              to_dir])
+    return (result == 0)  # zero exit on success
+
+def delay():
+    # Wait 60 seconds
+    print "Sleeping for 60 seconds",
+    sys.stdout.flush()
+    for i in range(6):
+        time.sleep(10)
+        print "...%d" % (60 - ((i + 1) * 10)),
+        sys.stdout.flush()
+    print ""
+
+
+if __name__ == '__main__':
+    while True:
+        # Sync the Databuffer to the Remote
+        print "Syncing %s to %s..." % (Local_dir, Remote_dir)
+        success = rsync(Local_dir, Remote_dir, "-q", "-W", "--progress", "-h")
+        if not success:
+            print "Syncing Local to Remote sync failed.  Retrying..."
+
+        delay()
