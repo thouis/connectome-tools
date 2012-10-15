5b7ba066ecd59c3ebaa5c5c34bbd6276d3eaa489
Thouis (Ray) Jones <thouis@seas.harvard.edu>
2012-10-15 10:08:33 -0400

    only group permissions

--- remote_pusher.py
+++ remote_pusher.py
@@ -12,7 +12,7 @@ Remote_dir = '/cygdrive/x/joshm/LGNs1/rawMontages/'
 
 def rsync(from_dir, to_dir, *args):
     result = subprocess.call(["/usr/bin/rsync",
-                              "-a", "--chmod=Do+rx,Fo+r"] + list(args) +
+                              "-a", "--chmod=Dg+rwx,Fg+rw"] + list(args) +
                              [from_dir,
                               to_dir])
     # subprocess.call("setfacl -m o::r-x".split(' ') + [to_dir])
